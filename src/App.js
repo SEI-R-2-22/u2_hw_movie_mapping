130d38b51c9a2f2780d9cd5b12919c1a92ce6d8a
Jenna Leopold <jleopold424@gmail.com>
2022-03-15 19:33:34 -0400

    map over movies and genres

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './styles/App.css'
 import React from 'react'
+import Movie from './components/Movie'
+import Genre from './components/Genre'
+import movieArray from './data/movies.json'
 
 const App = () => {
   return (
     <div className="App">
       <h1>Movie List</h1>
+      {movieArray.map((movie, index) => (
+        <div key={index} className="movie-wrapper">
+          <Movie
+            title={movie.title}
+            releaseDate={movie.release_date}
+            overview={movie.overview}
+            posterPath={movie.poster_path}
+          />
+          {movie.genres.map((genre) => (
+            <Genre key={genre.id} name={genre.name} />
+          ))}
+        </div>
+      ))}
     </div>
   )
-};
+}
 
-export default App;
+export default App
