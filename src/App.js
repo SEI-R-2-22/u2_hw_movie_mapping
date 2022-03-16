45fe1c8f46c67a093f1ecfb46d2859e0c173c930
Abdullah Albayati <dark.king1154@gmail.com>
2022-03-15 23:10:50 -0500

    added the logic for Individual movie

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './styles/App.css'
 import React from 'react'
-
+import movieArray from './data/movies.json'
+import IndividualMovie from './components/IndividualMovie';
+import MovieGenres from './components/MovieGenres';
 const App = () => {
   return (
     <div className="App">
       <h1>Movie List</h1>
-    </div>
+      {movieArray.map((movie, index) => (
+        <IndividualMovie movie = {movie} key={movie.id}/>
+      ))}
+      </div>
   )
 };
 
 export default App;
+
