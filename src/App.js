9d973d14b23c4df6167a25afc6c132816a065018
Aqsa Saleheene <angieclrk21@gmail.com>
2022-03-20 21:47:53 -0400

    hw 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import './styles/App.css'
 import React from 'react'
-import movieArray from './data/movies.json'
-import Movie from './components/Movie'
+import movies from './data/movies.json'
+import Movie from './components/Movie';
 import Genre from './components/Genre'
 
-
 const App = () => {
-  console.log(movieArray)
   return (
     <div className="App">
       <h1>Movie List</h1>
-      {movieArray.map((movie, index) => (
-        <div>
-          <Movie
-            title={movie.title}
-            releaseDate={movie.release_date}
-            overview={movie.overview}
-            image={movie.poster_path}
-            genres={movie.genres}
-          />
-          {movie.genres.map((genre, index) => (
-            <Genre genre={genre.name} key={index} />
-          ))}
-          </div>
-      ))}
-      </div> 
-  )
-          };
+      {movies.map((movie)=>
+      <Movie
+      key={movies.genre}
+      title={movie.title}
+      release_date={movie.release_date}
+      overview={movie.overview}
+      poster={movie.poster_path}
+      gs={movie.genres}
+      />
+      
+      )}
 
+    </div>
+  )
+};
 export default App;
